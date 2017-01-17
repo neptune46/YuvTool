d3786e704a0e0d292e06a857e3d8cc2863b30d17
neptune <neptune@DESKTOP-R0UPQB8>
2017-01-17 20:00:35 +0800

    add a sample class for scaling/display image

--- yuvtool.cpp
+++ yuvtool.cpp
@@ -1,8 +1,100 @@
 #include "stdafx.h"
 #include "yuvtool.h"
 
-YuvTool::YuvTool(QWidget *parent)
-	: QMainWindow(parent)
+#include <qmath.h>
+
+const int imageSize = 100;
+
+QImage scale(const QString &imageFileName)
 {
-	ui.setupUi(this);
+    QImage image(imageFileName);
+    return image.scaled(QSize(imageSize, imageSize), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 }
+
+YuvTool::YuvTool(QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowTitle(tr("Image loading and scaling example"));
+    resize(800, 600);
+
+    imageScaling = new QFutureWatcher<QImage>(this);
+    connect(imageScaling, SIGNAL(resultReadyAt(int)), SLOT(showImage(int)));
+    connect(imageScaling, SIGNAL(finished()), SLOT(finished()));
+
+    openButton = new QPushButton(tr("Open Images"));
+    connect(openButton, SIGNAL(clicked()), SLOT(open()));
+
+    cancelButton = new QPushButton(tr("Cancel"));
+    cancelButton->setEnabled(false);
+    connect(cancelButton, SIGNAL(clicked()), imageScaling, SLOT(cancel()));
+
+    pauseButton = new QPushButton(tr("Pause/Resume"));
+    pauseButton->setEnabled(false);
+    connect(pauseButton, SIGNAL(clicked()), imageScaling, SLOT(togglePaused()));
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    buttonLayout->addWidget(openButton);
+    buttonLayout->addWidget(cancelButton);
+    buttonLayout->addWidget(pauseButton);
+    buttonLayout->addStretch();
+
+    imagesLayout = new QGridLayout();
+
+    mainLayout = new QVBoxLayout();
+    mainLayout->addLayout(buttonLayout);
+    mainLayout->addLayout(imagesLayout);
+    mainLayout->addStretch();
+    setLayout(mainLayout);
+}
+
+YuvTool::~YuvTool()
+{
+    imageScaling->cancel();
+    imageScaling->waitForFinished();
+}
+
+void YuvTool::open()
+{
+    // Cancel and wait if we are already loading images.
+    if (imageScaling->isRunning()) {
+        imageScaling->cancel();
+        imageScaling->waitForFinished();
+    }
+
+    // Show a file open dialog at QStandardPaths::PicturesLocation.
+    QStringList files = QFileDialog::getOpenFileNames(this, tr("Select Images"),
+        QStandardPaths::writableLocation(QStandardPaths::PicturesLocation),
+        "*.jpg *.png");
+
+    if (files.count() == 0)
+        return;
+
+    // Do a simple layout.
+    qDeleteAll(labels);
+    labels.clear();
+
+    QLabel *imageLabel = new QLabel;
+    imageLabel->setFixedSize(imageSize, imageSize);
+    imagesLayout->addWidget(imageLabel, 0, 0);
+    labels.append(imageLabel);
+
+    // Use mapped to run the thread safe scale function on the files.
+    imageScaling->setFuture(QtConcurrent::mapped(files, scale));
+
+    openButton->setEnabled(false);
+    cancelButton->setEnabled(true);
+    pauseButton->setEnabled(true);
+}
+
+void YuvTool::showImage(int num)
+{
+    labels[num]->setPixmap(QPixmap::fromImage(imageScaling->resultAt(num)));
+}
+
+void YuvTool::finished()
+{
+    openButton->setEnabled(true);
+    cancelButton->setEnabled(false);
+    pauseButton->setEnabled(false);
+}
+
--- yuvtool.h
+++ yuvtool.h
@@ -1,15 +1,26 @@
 #pragma once
 
-#include <QtWidgets/QMainWindow>
 #include "ui_yuvtool.h"
 
-class YuvTool : public QMainWindow
+#include <QtWidgets>
+#include <QtConcurrent>
+
+class YuvTool : public QWidget
 {
     Q_OBJECT
-
 public:
-    YuvTool(QWidget *parent = Q_NULLPTR);
-
+    YuvTool(QWidget *parent = 0);
+    ~YuvTool();
+    public Q_SLOTS:
+    void open();
+    void showImage(int num);
+    void finished();
 private:
-    Ui::YuvToolClass ui;
+    QPushButton *openButton;
+    QPushButton *cancelButton;
+    QPushButton *pauseButton;
+    QVBoxLayout *mainLayout;
+    QList<QLabel *> labels;
+    QGridLayout *imagesLayout;
+    QFutureWatcher<QImage> *imageScaling;
 };
